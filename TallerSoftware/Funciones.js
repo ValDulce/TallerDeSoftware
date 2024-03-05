2b75aeb7fd5096d087730fa50ec3b2009ce12251
ValDulce <dulcearredondo0112@gmail.com>
2024-03-04 22:33:47 -0600

    a2

--- TallerSoftware/Funciones.js
+++ TallerSoftware/Funciones.js
@@ -1,6 +1,13 @@
 let carrito = [];
 let total = 0;
 
+function addToCart(productName) {
+    cartProducts.push(productName);
+    updateCartCount();
+    // Guardar los productos en el almacenamiento local
+    localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+}
+
 function agregarAlCarrito(idProducto, precio) {
     const indiceProducto = carrito.findIndex(item => item.id === idProducto);
 
@@ -12,7 +19,7 @@ function agregarAlCarrito(idProducto, precio) {
 
     total += precio;
     actualizarCarrito();
-}
+} 
 
 function quitarDelCarrito(idProducto, precio) {
     const indiceProducto = carrito.findIndex(item => item.id === idProducto);
@@ -46,3 +53,41 @@ function actualizarCarrito() {
     // Actualizar total
     totalElemento.textContent = `$${total.toFixed(2)}`;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtenemos el contenedor del carrito y el contador
+    var userCart = document.getElementById("user-cart");
+    var cartCount = document.getElementById("cart-count");
+
+    // Verificar si hay productos en el almacenamiento local
+    var storedCart = localStorage.getItem("cartProducts");
+    var cartProducts = storedCart ? JSON.parse(storedCart) : [];
+
+    // Función para agregar un producto al carrito
+    function addToCart(productName) {
+        cartProducts.push(productName);
+        updateCartCount();
+        // Guardar los productos en el almacenamiento local
+        localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+        localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+    }
+
+    // Función para actualizar el contador del carrito
+    function updateCartCount() {
+        var count = cartProducts.length;
+        cartCount.textContent = count;
+    }
+
+    // Agregamos eventos de clic a los botones "add-cart" en cada producto
+    var addCartButtons = document.querySelectorAll(".add-cart");
+    addCartButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var productName = this.closest(".content-card-product").querySelector("h3").textContent;
+            addToCart(productName);
+        });
+    });
+
+    // Actualizar el contador del carrito al cargar la página
+    updateCartCount();
+});
+
